ba0d8119ac7830b256a870cd0e5a81ad55b9a67f
Christopher Bishop <domingobishop@gmail.com>
2017-07-18 16:52:50 +0100

    date variables

--- functions-admin.php
+++ functions-admin.php
@@ -16,7 +16,7 @@ function woo_reports_admin_page() {
             <?php
             wp_nonce_field('woo_reports_admin');
             if (isset($_POST['submit-woo-reports']) && check_admin_referer('woo_reports_admin')) {
-                get_woo_orders();
+                get_woo_orders_by_date();
             }
             ?>
             <p class="submit">
@@ -62,5 +62,51 @@ function get_woo_orders() {
 
         echo '</p>';
     }
+}
+
+function get_woo_orders_by_date( $start_date = '2012-07-18', $end_date = '2017-07-18', $date_type = 'date_created' ) {
+
+    $args = array(
+        'type' => 'shop_order',
+        'status' => 'completed',
+        'orderby' => 'modified',
+        'order' => 'DESC',
+        'return' => 'ids',
+        $date_type => $start_date.'...'.$end_date,
+    );
+    $orders = wc_get_orders( $args );
+
+    $total = array();
+
+    $payment_methods = array();
+
+    foreach($orders as $order_id){
+        $order = wc_get_order( $order_id );
+
+        $order_data = $order->get_data();
+
+        $order_total = $order_data['total'];
+        $order_payment_method = $order_data['payment_method_title'];
+
+        $order_payment_method_total = $total[$order_payment_method] + $order_total;
+
+        array_push($payment_methods, $order_payment_method);
+
+        $total[$order_payment_method] = $order_payment_method_total;
+
+    }
+
+    $payment_methods_count = array_count_values($payment_methods);
+
+    echo '<table width="100%">';
+    echo '<tr><th>Method</th><th>Count</th><th>Total</th></tr>';
+    foreach ( $payment_methods_count as $key => $value ) {
+        echo '<tr>';
+        echo '<td>'.$key.'</td>';
+        echo '<td>'.$value.'</td>';
+        echo '<td>'.money_format( '%(#10n', $total[$key]).'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
 }
 
