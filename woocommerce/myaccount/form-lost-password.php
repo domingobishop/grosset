37fc29a57873d3e51c30c54c04473736cda2c513
Christopher Bishop <domingobishop@gmail.com>
2018-11-10 13:28:29 +0000

    upgrade completed order email

--- woocommerce/myaccount/form-lost-password.php
+++ woocommerce/myaccount/form-lost-password.php
@@ -22,7 +22,7 @@ defined( 'ABSPATH' ) || exit;
 <form method="post" class="woocommerce-ResetPassword lost_reset_password">
 
     <p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Welcome to Grosset Wines Club brand new <strong>secure sign in</strong>. To get you started we need to set your password. Please enter your members email address. You will receive a link to create a new password via email. <a href="https://www.grosset.com.au/contact/Grosset-Wine-Club-Member/">Not a member? Join here</a>.', 'woocommerce' ) ); ?></p>
-    
+
     <p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
         <label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?></label>
         <input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
