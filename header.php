17333baef45e10983b6d8a807a16edd3c23d7bfd
Christopher Bishop <domingobishop@gmail.com>
2019-01-26 12:40:40 +0000

    replace plugins with code

--- header.php
+++ header.php
@@ -29,6 +29,17 @@
 	<![endif]-->
 
 	<?php wp_head(); ?>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-102007187-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-102007187-1');
+    </script>
+    
 </head>
 
 <body <?php body_class(); ?>>
